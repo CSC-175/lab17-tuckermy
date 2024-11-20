e54a5f14b8c857dd3b44e4b3be1512b7c12f8e9f
Tucker Myers <tuckermy.umich.edu>
2024-11-20 10:22:18 -0500

    First Attempt

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,11 @@
 // Definition of findHigh function goes here
+
+template < typename A, typename B, typename C, typename D>
+void findHigh(A fpData[], B size, C &index, D &fpHigh) {
+    for (int i = 0; i < size; i++) {
+        if (fpData[i] > fpHigh) {
+            index = i;
+            fpHigh = fpData[i];
+        }
+    }
+}
